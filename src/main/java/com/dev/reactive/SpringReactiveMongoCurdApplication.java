ec97a7b869c08acc678f9a6301f047eab4aec1b2
Vũ Văn Thanh <vuthanhtb.dev@gmail.com>
2022-04-09 15:53:28 +0700

    swagger demo

--- src/main/java/com/dev/reactive/SpringReactiveMongoCurdApplication.java
+++ src/main/java/com/dev/reactive/SpringReactiveMongoCurdApplication.java
@@ -1,9 +1,16 @@
 package com.dev.reactive;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Spring Webflux CURD example",
+        version = "1.0",
+        description = "demo swagger in spring webflux"
+))
 public class SpringReactiveMongoCurdApplication {
 
     public static void main(String[] args) {
